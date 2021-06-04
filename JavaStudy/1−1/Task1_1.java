5937dba067f118fb63a8476ebb825bb89447d49d
tk-956 <84982466+tk-956@users.noreply.github.com>
2021-06-05 01:33:01 +0900

    課題提出
    １章データ型・文字列クラス

---
+++ JavaStudy/1−1/Task1_1.java
@@ -0,0 +1,15 @@
+public class Task1_1{
+
+    public static void main(String[] args) {
+        int hogeInt = 50;
+        String moji = "hogeString";
+        //double型hogeDouble変数の初期値は0.2
+        double hogeDouble = 0.2;
+        hogeInt = 100;
+        moji ="合格";
+        System.out.println(hogeInt);
+        //変数hogeStringの値表示
+        System.out.println(moji);
+        System.out.println(hogeDouble);
+    }
+}
