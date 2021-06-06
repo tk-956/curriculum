fa4780352ed7de0ab4d7ebbc0cc1f967625e373e
tk-956 <84982466+tk-956@users.noreply.github.com>
2021-06-06 18:11:21 +0900

    提出用課題
    １章　配列　1−２

---
+++ JavaStudy/1-2/Task1_2.java
@@ -0,0 +1,26 @@
+public class Task1_2 {
+    
+    public static void main(String[] args) {
+
+        String[] country = {"JAPAN","AMERICA","KOREA"};
+        System.out.println(country);
+
+        String[] strArray = {"りんご","もも","ぶどう"};
+        System.out.println(strArray[1]);
+        
+        //10,20,30,40,50を要素の値（初期値）とする、要素数５の
+        //intArrayという名前のint型配列を宣言。
+        int[] intArrey = {10,20,30,40,50};
+
+        //intArrayの②番目と⑤番目の要素値を出力
+        System.out.println(intArrey[1]+intArrey[4]);
+
+
+    }
+
+
+
+
+
+
+}
--- JavaStudy/1−1/Task1_1.java
+++ JavaStudy/1−1/Task1_1.java
@@ -6,7 +6,7 @@ public class Task1_1{
         //double型hogeDouble変数の初期値は0.2
         double hogeDouble = 0.2;
         hogeInt = 100;
-        moji ="合格";
+        moji = "合格";
         System.out.println(hogeInt);
         //変数hogeStringの値表示
         System.out.println(moji);
